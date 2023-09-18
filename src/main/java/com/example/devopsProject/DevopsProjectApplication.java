49c7fbff2de1e4e574dea621c5d7302c220b1be4
RaghuvanshiHarjit0001 <93963645+RaghuvanshiHarjit0001@users.noreply.github.com>
2023-09-18 10:57:29 -0400

    Initial Commit

---
+++ src/main/java/com/example/devopsProject/DevopsProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.devopsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DevopsProjectApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
